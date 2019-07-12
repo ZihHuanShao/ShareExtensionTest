8251371cdf3bd14dc6b29af013ff1d8d4a2b493f
ruby <ruby@maxkit.com.tw>
2019-07-12 11:40:33 +0800

    app端改UI，拉tableView

--- ShareExtensionTest/ViewController/ViewController.swift
+++ ShareExtensionTest/ViewController/ViewController.swift
@@ -10,13 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var myImageView: UIImageView!
-    @IBOutlet weak var myLabel: UILabel!
+    @IBOutlet weak var myTableView: UITableView!
     
     let manager     = FavoriteManager.shareInstance
+    let fileObjects = FavoriteManager.shareInstance.getFavoriteData()
     let suiteName   = "group.maxkit.fred.ShareExtensionTest"
     
     override func viewDidLoad() {
+        
         super.viewDidLoad()
+        updateSrc()
         
         guard let fileObjects = manager.getFavoriteData() else { return }
         
@@ -72,54 +75,51 @@ class ViewController: UIViewController {
         }
         */
     }
-
+    
     override func viewWillAppear(_ animated: Bool) {
-        
-        
-        /*
-        // MARK: - Support type: kUTTypeImage
-        // METHOD 1: 讀取失敗，UIImage(contentsOfFile: imagePath.path) always回傳nil
-        if let shareUrl = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: "group.maxkit.fred.ShareExtensionTest") {
-            let imagePath = shareUrl.appendingPathComponent("IMG_2093.JPG")
-            print("imagePath: \(imagePath)")
-            print("imagePath.path: \(imagePath.path)")
-            let image = UIImage(contentsOfFile: imagePath.path)
-            
-            DispatchQueue.main.async {
-                self.myImageView.image = image
-            }
-        }
-        */
-        
-        
-        
-        /*
-        // MARK: - Support type: kUTTypeImage
-        // METHOD 2: 讀取OK
-        if let userDefault = UserDefaults.init(suiteName: "group.maxkit.fred.ShareExtensionTest") {
 
-            print("viewWillAppear")
-            if let tmpData = userDefault.object(forKey: "tmpJPG") as? Data {
-                print("viewWillAppear again")
-                let encodeObject = try? PropertyListDecoder().decode(Data.self, from: tmpData)
-                DispatchQueue.main.async {
-                    self.myImageView.image = UIImage(data: encodeObject!)
-                }
-            }
-            
-        }
-        if let tempPath = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: "group.maxkit.fred.ShareExtensionTest") {
-            do{
-                let fileList = try FileManager.default.contentsOfDirectory(atPath: tempPath.path)
-                for file in fileList{
-                    print(file)
-                }
-            }
-            catch{
-                print("Cannot list directory")
-            }
-        }
-        */
     }
+    
+    func updateSrc() {
+        myTableView.dataSource = self
+        myTableView.delegate = self
+    }
+    
+    
 }
 
+// MRAK: - UITableViewDataSource
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if let _ = fileObjects {
+            return fileObjects!.count
+        }
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CELL", for: indexPath) as! myTableViewCell
+        if let _ = fileObjects {
+            cell.textLabel!.text = fileObjects![indexPath.row].name
+        }
+        
+        return cell
+    }
+    
+   
+}
+
+// MRAK: - UITableViewDelegate
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let _ = fileObjects {
+            
+            
+            
+            /*
+            let activityViewController = UIActivityViewController(activityItems: [fileObjects![indexPath.row].url.path], applicationActivities: nil)
+            self.present(activityViewController, animated: true, completion: nil)
+            */
+        }
+    }
+}
